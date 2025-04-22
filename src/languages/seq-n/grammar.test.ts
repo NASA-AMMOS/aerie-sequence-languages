469d96f9325e000daabc1fb354fdb2269ad20f7a
Carter Mak <Carter.Mak@jpl.nasa.gov>
2025-04-22 09:29:54 -0700

    Rename parser

--- src/languages/seq-n/grammar.test.ts
+++ src/languages/seq-n/grammar.test.ts
@@ -1,5 +1,5 @@
 import { testTree } from '@lezer/generator/dist/test';
-import { SeqParser } from './seq-n';
+import { SeqnParser } from './seq-n';
 import { describe, expect, test } from 'vitest';
 
 /*
@@ -583,6 +583,6 @@ describe.each([
 
     testTree will throw if there's a mismatch between the returned actual and expected trees, it returns
     undefined when they match. */
-    expect(testTree(SeqParser.parse(input), expected, undefined)).toBeUndefined();
+    expect(testTree(SeqnParser.parse(input), expected, undefined)).toBeUndefined();
   });
 });
