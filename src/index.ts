5be906ba79f4b013cf6589078480db4066c066a8
Carter Mak <Carter.Mak@jpl.nasa.gov>
2025-04-24 08:33:10 -0700

    Export SeqN constants

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './converters/seqJsonToSeqn';
 export * from './converters/seqnToSeqJson';
 export * from './languages/seq-n/seq-n';
+export { SEQN_CONSTANTS } from './languages/seq-n/seqn-grammar-constants';
