6ee8d0f7df1112e0510b6d64ad730e1af5180e65
rrgoetz <ryan.r.goetz@jpl.nasa.gov>
2025-04-28 08:49:53 -1000

    Update seqn-grammar constants

--- src/languages/seq-n/seqn-grammar-constants.ts
+++ src/languages/seq-n/seqn-grammar-constants.ts
@@ -3,7 +3,9 @@ export const RULE_SEQUENCE_NAME = 'SequenceName';
 export const RULE_GROUND_NAME = 'GroundName';
 export const RULE_STEM = 'Stem';
 export const RULE_REQUEST_NAME = 'RequestName';
+export const RULE_COMMANDS = 'Commands';
 export const RULE_COMMAND = 'Command';
+export const RULE_STEPS = 'Steps';
 export const RULE_ACTIVATE = 'Activate';
 export const RULE_GROUND_BLOCK = 'GroundBlock';
 export const RULE_GROUND_EVENT = 'GroundEvent';
@@ -11,17 +13,30 @@ export const RULE_LOAD = 'Load';
 export const RULE_REQUEST = 'Request';
 export const RULE_REPEAT_ARG = 'RepeatArg';
 export const RULE_ENUM = 'Enum';
+export const RULE_ENUM_NAME = 'EnumName';
+export const RULE_RANGE = 'Range';
+export const RULE_TYPE = 'Type';
+export const RULE_OBJECT = 'Object';
 export const RULE_METADATA = 'Metadata';
 export const RULE_METADATA_ENTRY = 'MetaEntry';
 export const RULE_ID_DECLARATION = 'IdDeclaration';
 export const RULE_PARAMETER_DECLARATION = 'ParameterDeclaration';
 export const RULE_LOCAL_DECLARATION = 'LocalDeclaration';
 export const RULE_VARIABLE = 'Variable';
-
+export const RULE_VALUES = 'Values';
+export const RULE_PROPERTY = 'Property';
+export const RULE_PROPERTY_VALUE = 'PropertyValue';
+export const RULE_PROPERTY_NAME = 'PropertyName';
+export const RULE_NTEXT = 'NTEXT';
 export const TOKEN_STRING = 'String';
 export const TOKEN_NUMBER = 'Number';
 export const TOKEN_ERROR = '⚠';
 export const TOKEN_LINE_COMMENT = 'LineComment';
+export const VAR_ENUM = 'ENUM';
+export const VAR_STRING = 'STRING';
+export const VAR_UINT = 'UINT';
+export const VAR_FLOAT = 'FLOAT';
+export const VAR_INT = 'INT';
 
 export const SEQN_CONSTANTS = {
     RULE_ARGS,
@@ -29,7 +44,9 @@ export const SEQN_CONSTANTS = {
     RULE_GROUND_NAME,
     RULE_STEM,
     RULE_REQUEST_NAME,
+    RULE_COMMANDS,
     RULE_COMMAND,
+    RULE_STEPS,
     RULE_ACTIVATE,
     RULE_GROUND_BLOCK,
     RULE_GROUND_EVENT,
@@ -37,14 +54,27 @@ export const SEQN_CONSTANTS = {
     RULE_REQUEST,
     RULE_REPEAT_ARG,
     RULE_ENUM,
+    RULE_ENUM_NAME,
+    RULE_RANGE,
     RULE_METADATA,
     RULE_METADATA_ENTRY,
     RULE_ID_DECLARATION,
     RULE_PARAMETER_DECLARATION,
     RULE_LOCAL_DECLARATION,
     RULE_VARIABLE,
+    RULE_VALUES,
+    RULE_NTEXT,
+    RULE_OBJECT,
+    RULE_TYPE,
+    RULE_PROPERTY,
+    RULE_PROPERTY_NAME,
     TOKEN_STRING,
     TOKEN_NUMBER,
     TOKEN_ERROR,
-    TOKEN_LINE_COMMENT
+    TOKEN_LINE_COMMENT,
+    VAR_ENUM,
+    VAR_STRING,
+    VAR_UINT,
+    VAR_FLOAT,
+    VAR_INT
 }
