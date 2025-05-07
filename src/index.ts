025cba12d74df80d045ef5fb3659721e13af5233
rrgoetz <ryan.r.goetz@jpl.nasa.gov>
2025-05-07 05:19:52 -1000

    I think I made the wildcard exports work now

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-export { seqJsonToSeqn, } from './converters/seqJsonToSeqn';
-export {seqnToSeqJson, parseVariables} from './converters/seqnToSeqJson';
-export {SeqnParser} from './languages/seq-n/seq-n';
-export { SEQN_NODES } from './languages/seq-n/seqn-grammar-constants';
+export * from './converters/seqJsonToSeqn.js';
+export * from './converters/seqnToSeqJson.js';
+export * from './languages/seq-n/seq-n.js';
+export * from './languages/seq-n/seqn-grammar-constants.js';
+export * from './utils/string.js'
 
-export {ParsedSeqn, ParseSasf, ParsedSatf, Seqn} from './languages/satf/types/types';
-export { sasfToSeqn, satfToSeqn, seqnToSASF, seqnToSATF} from './converters/satf-sasf-utils';
-export { SatfSasfParser } from './languages/satf/grammar/satf-sasf'
-export { SATF_SASF_NODES } from './languages/satf/constants/satf-sasf-constants';
+export * from './languages/satf/types/types.js';
+export * from './converters/satf-sasf-utils.js';
+export * from './languages/satf/grammar/satf-sasf.js'
+export * from './languages/satf/constants/satf-sasf-constants.js';
