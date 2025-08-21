7a519b374b9edcc34e25996c90b6073c9e83c5d1
Carter Mak <Carter.Mak@jpl.nasa.gov>
2025-08-21 13:03:46 -0700

    First pass at exports for aerie-ui

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,49 @@ export type { ParsedSeqn, Seqn, ParsedSatf, ParseSasf } from './languages/satf/t
 export { seqnToSATF, seqnToSASF, satfToSeqn, sasfToSeqn } from './converters/satf-sasf-utils.js';
 export { SatfSasfParser, SatfLanguage } from './languages/satf/grammar/satf-sasf.js';
 export { SATF_SASF_NODES } from './languages/satf/constants/satf-sasf-constants.js';
+
+export {
+  type UserSequence,
+  type LibrarySequence,
+  type LibrarySequenceMap,
+  type PhoenixContext,
+  type LanguageAdaptation,
+  type OutputLanguageAdaptation,
+  type NewAdaptationInterface,
+  SequenceTypes,
+} from './adaptation/interfaces/new-adaptation-interface.js';
+
+export type {
+  TimeTagInfo,
+  StringArg,
+  ArgTextDef,
+  CommandInfoMapper,
+} from './adaptation/interfaces/command-info-mapper.js';
+
+export {
+  fswCommandArgDefault,
+  isFswCommand,
+  isHwCommand,
+  isFswCommandArgumentEnum,
+  isFswCommandArgumentInteger,
+  isFswCommandArgumentFloat,
+  isFswCommandArgumentNumeric,
+  isFswCommandArgumentUnsigned,
+  isFswCommandArgumentRepeat,
+  isFswCommandArgumentVarString,
+  isFswCommandArgumentFixedString,
+  isFswCommandArgumentBoolean,
+  isHexValue,
+  addDefaultArgs,
+  getDefaultVariableArgs,
+  addDefaultVariableArgs,
+  parseNumericArg,
+  decodeInt32Array,
+  getAllEnumSymbols,
+} from './utils/sequence-utils.js';
+
+export { seqnAdaptation } from './adaptation/languages/seq-n/language.js';
+
+export { seqJsonOutputAdaptation } from './adaptation/languages/seqjson/language.js';
+
+export { parseCdlDictionary, toAmpcsXml } from './adaptation/languages/vml/cdl-dictionary.js';
